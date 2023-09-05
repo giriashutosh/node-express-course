45c6885e3d3f7a6673cb05cae782e8d48963e14b
Ashutosh Giri <ashutoshgiri249@gmail.com>
2023-09-05 12:56:01 +0530

    updates

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,8 @@
-console.log('Express Tutorial')
+const http = require('http')
+
+const server = http.createServer ((req,res) => {
+    console.log("User hit the server")
+    res.end("Hyy, welcome to server")
+})
+
+server.listen(5000)
